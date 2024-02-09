be5723c7fc822eaf9c3a3ee71cffc792d0599eb5
LalitthaRavi <lalitha.rgs@gmail.com>
2024-02-09 23:33:16 +0800

    Updated code with plots

--- MachineLearningCourse/Assignments/Module01/Framework-7-ModelTuning_SMSSpam.py
+++ MachineLearningCourse/Assignments/Module01/Framework-7-ModelTuning_SMSSpam.py
@@ -7,7 +7,7 @@ sys.path.append(projDir) #look in the directory containing MachineLearningCourse
 sys.path.append(curDir)  #look in the directory of this file too, i.e., Module01/
 
 #specify the directory to store your visualization files
-kOutputDirectory = "/users/stanleykok"  #use this for Mac or Linux
+kOutputDirectory = "/Users/dhanushlalitha/Documents/GIT/IS/IS5126_Guided_Project/MachineLearningCourse/Plots//"  #use this for Mac or Linux
 #kOutputDirectory = "C:\\temp\\visualize" #use this for Windows
 
 import MachineLearningCourse.MLProjectSupport.SMSSpam.SMSSpamDataset as SMSSpamDataset
@@ -54,48 +54,231 @@ import MachineLearningCourse.MLUtilities.Visualizations.Charting as Charting
 #    for later tabulation and charting...
 def ExecuteEvaluationRun(runSpecification, xTrainRaw, yTrain, numberOfFolds = 5):
     startTime = time.time()
-    
-    # HERE upgrade this to use crossvalidation
-    
+    accuracy_values = []
+
+    # HERE upgrade this to use cross validation
     featurizer = SMSSpamFeaturize.SMSSpamFeaturize()
-    featurizer.CreateVocabulary(xTrainRaw, yTrain, numFrequentWords = runSpecification['numFrequentWords'], numMutualInformationWords = runSpecification['numMutualInformationWords'])
+    featurizer.CreateVocabulary(xTrainRaw, yTrain, numFrequentWords=runSpecification['numFrequentWords'],
+                                numMutualInformationWords=runSpecification['numMutualInformationWords'])
 
-    xTrain      = featurizer.Featurize(xTrainRaw)
-    xValidate   = featurizer.Featurize(xValidateRaw)
+    xTrain = featurizer.Featurize(xTrainRaw)
+    xValidate = featurizer.Featurize(xValidateRaw)
 
-    model = LogisticRegression.LogisticRegression()
-    model.fit(xTrain,yTrain,convergence=runSpecification['convergence'], stepSize=runSpecification['stepSize'], verbose=True)
-    
-    validationSetAccuracy = EvaluateBinaryClassification.Accuracy(yValidate, model.predict(xValidate))
-    
-    runSpecification['accuracy'] = validationSetAccuracy
+    # K-fold cross validation
+    if numberOfFolds > 1:
 
-    # HERE upgrade this to calculate and save some error bounds...
-    
+        for foldId in range(numberOfFolds):
+            xTrain_fold, yTrain_fold, xEvaluate_fold, yEvaluate_fold = CrossValidation.CrossValidation(xTrain,
+                                                                                                       yTrain,
+                                                                                                       numberOfFolds,
+                                                                                                       foldId)
+
+            model = LogisticRegression.LogisticRegression()
+            model.fit(xTrain_fold, yTrain_fold, convergence=runSpecification['convergence'], stepSize=runSpecification['stepSize'],
+                      verbose=True)
+
+            validationSetAccuracy = EvaluateBinaryClassification.Accuracy(yEvaluate_fold, model.predict(xEvaluate_fold))
+            accuracy_values.append(validationSetAccuracy)
+
+        mean = sum(accuracy_values) / numberOfFolds
+        runSpecification['accuracy'] = mean
+        lower, upper = ErrorBounds.GetAccuracyBounds(runSpecification['accuracy'], len(yEvaluate_fold), 0.5)
+        runSpecification['accuracyErrorBound'] = mean - lower
+
+    # Train on the entire training set and evaluate on the validation set
+    elif numberOfFolds == 1:
+        model = LogisticRegression.LogisticRegression()
+        model.fit(xTrain, yTrain,
+                  convergence=runSpecification['convergence'],
+                  stepSize=runSpecification['stepSize'], verbose=True)
+        validationSetAccuracy = EvaluateBinaryClassification.Accuracy(yValidate, model.predict(xValidate))
+        runSpecification['accuracy'] = validationSetAccuracy
+        confidence = 0.5
+        lower, upper = ErrorBounds.GetAccuracyBounds(runSpecification['accuracy'], len(yValidate), confidence)
+        runSpecification['accuracyErrorBound'] = validationSetAccuracy - lower
     endTime = time.time()
     runSpecification['runtime'] = endTime - startTime
-    
     return runSpecification
 
-evaluationRunSpecifications = []
-for numMutualInformationWords in [10, 25, 50, 75, 100]:
+# Part 1: Defining the hyperparameter values to sweep  #larger ones then shift to smaller ones
+step_values = [2, 1.75, 1.5, 1.25, 1.0, 0.75, 0.5]
+convergence_values = [0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0001]
+numMutualInformationWords_values = [300, 150, 100, 50, 20, 0]
+numFrequentWords_values = [300, 150, 100, 50, 20, 0]
 
-    runSpecification = {}
-    runSpecification['optimizing'] = 'numMutualInformationWords'
-    runSpecification['numMutualInformationWords'] = numMutualInformationWords
-    runSpecification['stepSize'] = 1.0
-    runSpecification['convergence'] = 0.005
-    runSpecification['numFrequentWords'] = 0
-    
-    evaluationRunSpecifications.append(runSpecification)
 
-## if you want to run in parallel you need to install joblib as described in the lecture notes and adjust the comments on the next three lines...
-#from joblib import Parallel, delayed
-#evaluations = Parallel(n_jobs=12)(delayed(ExecuteEvaluationRun)(runSpec, xTrainRaw, yTrain) for runSpec in evaluationRunSpecifications)
+numberOfFolds = 2
+hyperparams = {'stepSize': step_values, 'convergence': convergence_values, 'numFrequentWords': numFrequentWords_values, 'numMutualInformationWords': numMutualInformationWords_values}
 
-evaluations = [ ExecuteEvaluationRun(runSpec, xTrainRaw, yTrain) for runSpec in evaluationRunSpecifications ]
+def parameterSweepRunSpecifications(hyper_param, initialRunSpecification):
+    hyper_param_key, hyper_param_values = hyper_param
+    print(hyper_param_key, hyper_param_values)
+    evaluationRunSpecifications = []
+    if hyper_param_key == 'stepSize':
+        for hyper_param_value in hyper_param_values:
+            runSpecification = {}
+            runSpecification['optimizing'] = hyper_param_key
+            runSpecification['stepSize'] = hyper_param_value
+            runSpecification['convergence'] = initialRunSpecification['convergence']
+            runSpecification['numFrequentWords'] = initialRunSpecification['numFrequentWords']
+            runSpecification['numMutualInformationWords'] = initialRunSpecification['numMutualInformationWords']
+            evaluationRunSpecifications.append(runSpecification)
+    elif hyper_param_key == 'convergence':
+        for hyper_param_value in hyper_param_values:
+            runSpecification = {}
+            runSpecification['optimizing'] = hyper_param_key
+            runSpecification['convergence'] = hyper_param_value
+            runSpecification['numFrequentWords'] = initialRunSpecification['numFrequentWords']
+            runSpecification['numMutualInformationWords'] = initialRunSpecification['numMutualInformationWords']
+            runSpecification['stepSize'] = initialRunSpecification['stepSize']
+            evaluationRunSpecifications.append(runSpecification)
+    elif hyper_param_key == 'numFrequentWords':
+        for hyper_param_value in hyper_param_values:
+            runSpecification = {}
+            runSpecification['optimizing'] = hyper_param_key
+            runSpecification['numFrequentWords'] = hyper_param_value
+            runSpecification['stepSize'] = initialRunSpecification['stepSize']
+            runSpecification['convergence'] = initialRunSpecification['convergence']
+            runSpecification['numMutualInformationWords'] = initialRunSpecification['numMutualInformationWords']
+            evaluationRunSpecifications.append(runSpecification)
+    elif hyper_param_key == 'numMutualInformationWords':
+        for hyper_param_value in hyper_param_values:
+            runSpecification = {}
+            runSpecification['optimizing'] = hyper_param_key
+            runSpecification['numMutualInformationWords'] = hyper_param_value
+            runSpecification['stepSize'] = initialRunSpecification['stepSize']
+            runSpecification['convergence'] = initialRunSpecification['convergence']
+            runSpecification['numFrequentWords'] = initialRunSpecification['numFrequentWords']
+            evaluationRunSpecifications.append(runSpecification)
+    return evaluationRunSpecifications
 
-for evaluation in evaluations:
-    print(evaluation)
-    
-# Good luck!
+def findOptimalSpecs(specs):
+    specs = sorted(specs, key=lambda x: x['accuracy'])
+    best_specs = specs[0]
+    for i in range(1, len(specs)):
+
+        bestUpperBound = best_specs['accuracy'] + specs[i]['accuracyErrorBound']
+        # Part 3a: the value you pick is tied with the highest accuracy value according to a 75% 1-sided bound
+        if specs[i]['accuracy'] > bestUpperBound:
+            best_specs = specs[i]
+        # Part 3b: the value you pick has the lowest runtime among these ‘tied’ values.
+        elif specs[i]['accuracy'] > best_specs['accuracy'] and specs[i]['runtime'] < best_specs['runtime']:
+            best_specs = specs[i]
+
+    return {
+        'stepSize': best_specs['stepSize'],
+        'convergence': best_specs['convergence'],
+        'numFrequentWords':  best_specs['numFrequentWords'],
+        'numMutualInformationWords': best_specs['numMutualInformationWords']
+    }
+
+
+
+
+
+
+initialHyperParams = optimalHyperParams = {'stepSize': 1.0, 'convergence': 0.005, 'numFrequentWords': 0, 'numMutualInformationWords': 20}
+
+#######################################################################################################################
+# Part 2: Optimal hyperparameters ( Independent of other hyperparameters)
+for sweep_number, hyper_param in enumerate(hyperparams.items(), start=1): # 1 sweep for each hyperparameter
+    hyper_param_key, hyper_param_values = hyper_param
+    print("Optimising hyperparameter: --->", hyper_param_key)
+    evaluationRunSpecifications = parameterSweepRunSpecifications(hyper_param, optimalHyperParams)
+    evaluations = [ExecuteEvaluationRun(runSpec, xTrainRaw, yTrain) for runSpec in evaluationRunSpecifications]
+
+    for evaluation in evaluations:
+        print(evaluation)
+
+    # Extracting data for plotting
+    accuracy_values = [evaluation['accuracy'] for evaluation in evaluations]
+    accuracy_errors = [evaluation['accuracyErrorBound'] for evaluation in evaluations]
+    runtime_values = [evaluation['runtime'] for evaluation in evaluations]
+
+    # Plotting the accuracy
+    Charting.PlotSeriesWithErrorBars([accuracy_values], [accuracy_errors], [hyper_param_key],
+                                     hyperparams[hyper_param_key], chartTitle="Accuracy vs " + hyper_param_key,
+                                     xAxisTitle=hyper_param_key, yAxisTitle="Accuracy", yBotLimit=0.8,
+                                     outputDirectory=kOutputDirectory, fileName="Part_2_Accuracy_vs_" + hyper_param_key)
+
+    # Plotting the runtime
+    Charting.PlotSeries([runtime_values], [hyper_param_key], hyperparams[hyper_param_key],
+                        chartTitle="Runtime vs " + hyper_param_key, xAxisTitle=hyper_param_key,
+                        yAxisTitle="Runtime (seconds)", outputDirectory=kOutputDirectory,
+                        fileName="Part_2_Runtime_vs_" + hyper_param_key)
+
+#######################################################################################################################
+# Part 3: Optimal hyperparameters (Dependent on other hyperparameters)
+
+init_specs = ExecuteEvaluationRun(optimalHyperParams, xTrainRaw, yTrain, 1)
+validation_accuracy_across_sweeps = [init_specs['accuracy']] # initial accuracy
+validation_accuracy_errors_across_sweeps = [init_specs['accuracyErrorBound']] # initial accuracy error
+for sweep_number, hyper_param in enumerate(hyperparams.items(), start=1):
+    hyper_param_key, hyper_param_values = hyper_param
+    print("Optimising hyperparameter: --->", hyper_param_key)
+    evaluationRunSpecifications = parameterSweepRunSpecifications(hyper_param, optimalHyperParams)
+    evaluations = [ExecuteEvaluationRun(runSpec, xTrainRaw, yTrain, 2) for runSpec in evaluationRunSpecifications] #hyper paramter search with 2 fold cross validation
+
+    for evaluation in evaluations:
+        print(evaluation)
+
+    optimalHyperParams = findOptimalSpecs(evaluations)
+
+    # Print the optimal specs for current sweep
+    print("#"*50)
+    print(f"Optimal hyper parameters after sweep {sweep_number}: ", optimalHyperParams)
+
+    # Part 4a: Evaluating the accuracy on the validation set after optimising each hyperparameter
+    result = ExecuteEvaluationRun(optimalHyperParams, xTrainRaw, yTrain, 1)
+    validation_accuracy_across_sweeps.append(result['accuracy'])
+    validation_accuracy_errors_across_sweeps.append(result['accuracyErrorBound'])
+    sweep_list = [i for i in range(1, sweep_number+2)] # 1 to sweep_number+1(initial accuracy)
+    # Part 4b: Plotting the validation accuracy across the sweeps
+    Charting.PlotSeriesWithErrorBars([validation_accuracy_across_sweeps], [validation_accuracy_errors_across_sweeps],
+                                     ["Accuracy"],
+                                     sweep_list, chartTitle=f"Accuracy across Sweep: {sweep_number}",
+                                     xAxisTitle="Sweep", yAxisTitle="Accuracy", yBotLimit=0.8,
+                                     outputDirectory=kOutputDirectory, fileName=f"Sweep_{sweep_number}_Accuracy")
+
+
+# Best hyperarameters after all sweeps
+print(f"Best hyper parameters after all sweeps: ", optimalHyperParams)
+
+
+# Part 6: ROC of initial VS Best model
+seriesFPRs = []
+seriesFNRs = []
+seriesLabels = []
+
+
+featurizer = SMSSpamFeaturize.SMSSpamFeaturize()
+featurizer.CreateVocabulary(xTrainRaw, yTrain, numFrequentWords = initialHyperParams['numFrequentWords'], numMutualInformationWords = initialHyperParams['numMutualInformationWords'])
+
+xTrain = featurizer.Featurize(xTrainRaw)
+xTest = featurizer.Featurize(xTestRaw)
+
+init_model = LogisticRegression.LogisticRegression()
+init_model.fit(xTrain, yTrain, convergence=initialHyperParams['convergence'], stepSize=initialHyperParams['stepSize'], verbose=False)
+EvaluateBinaryClassification.ExecuteAll(yTest, init_model.predict(xTest))
+(init_modelFPRs, init_modelFNRs, init_thresholds) = TabulateModelPerformanceForROC(init_model, xTest, yTest)
+seriesFPRs.append(init_modelFPRs)
+seriesFNRs.append(init_modelFNRs)
+seriesLabels.append('initial hyper parameters')
+
+featurizer = SMSSpamFeaturize.SMSSpamFeaturize()
+featurizer.CreateVocabulary(xTrainRaw, yTrain, numFrequentWords = optimalHyperParams['numFrequentWords'], numMutualInformationWords = optimalHyperParams['numMutualInformationWords'])
+
+xTrain = featurizer.Featurize(xTrainRaw)
+xTest = featurizer.Featurize(xTestRaw)
+
+best_model = LogisticRegression.LogisticRegression()
+best_model.fit(xTrain, yTrain, convergence=optimalHyperParams['convergence'], stepSize=optimalHyperParams['stepSize'], verbose=False)
+EvaluateBinaryClassification.ExecuteAll(yTest, best_model.predict(xTest))
+
+(best_modelFPRs, best_modelFNRs, best_thresholds) = TabulateModelPerformanceForROC(best_model, xTest, yTest)
+seriesFPRs.append(best_modelFPRs)
+seriesFNRs.append(best_modelFNRs)
+seriesLabels.append('optimal hyper parameters')
+
+Charting.PlotROCs(seriesFPRs, seriesFNRs, seriesLabels, useLines=True, chartTitle="ROC Comparison", xAxisTitle="False Negative Rate", yAxisTitle="False Positive Rate", outputDirectory=kOutputDirectory, fileName="Best_VS_Initial_ROC")
+
--- MachineLearningCourse/Assignments/Module01/SMSSpamFeaturize.py
+++ MachineLearningCourse/Assignments/Module01/SMSSpamFeaturize.py
@@ -1,38 +1,99 @@
+from collections import Counter
+import numpy as np
+
 class SMSSpamFeaturize(object):
     """A class to coordinate turning SMS spam strings into feature vectors."""
 
     def __init__(self, useHandCraftedFeatures=False):
         # use hand crafted features specified in _FeaturizeXForHandCrafted()
         self.useHandCraftedFeatures = useHandCraftedFeatures
-        
+
         self.ResetVocabulary()
-        
+
     def ResetVocabulary(self):
         self.vocabularyCreated = False
         self.vocabulary = []
 
     def Tokenize(self, xRaw):
         return str.split(xRaw)
-    
+
     def FindMostFrequentWords(self, x, n):
         print("Stub FindMostFrequentWords in ", __file__)
+        # Use Counter object to count word frequencies
+        word_counts = Counter()
+        for text in x:
+            words = text.split()  # Tokenize by whitespace
+            word_counts.update(words)
+
+        # Return the top n most frequent words
+        return [word for word, _ in word_counts.most_common(n)]
 
     def FindTopWordsByMutualInformation(self, x, y, n):
         print("Stub FindTopWordsByMutualInformation in ", __file__)
 
-    def CreateVocabulary(self, xTrainRaw, yTrainRaw, supplementalVocabularyWords=[]):
+        """
+        Finds the top n words with the highest mutual information with the labels.
+        """
+
+        # Get the statistics to calculate joint probability of word with the labels
+        c = Counter()
+        c0 = Counter()
+        c1 = Counter()
+        for i in range(len(x)):
+            words = x[i].split()
+            counted = set()
+            for word in words:
+                if word not in counted:
+                    c[word] += 1
+                    if y[i] == 0:
+                        c0[word] += 1
+                    elif y[i] == 1:
+                        c1[word] += 1
+                counted.add(word)
+        # p(0) = count_sentences_with_label_0 / ( count_sentence_with_label_0 + count_sentence_with_label_1 )
+        # ...
+        p1 = sum(y) / len(y)
+        p0 = 1 - p1
+        mi = Counter()
+        for word in c:
+            # p(word) = count_sentences_with_word / ( count_sentence_with_word + count_sentence_without_word )
+            pWord = (c[word] + 1) / (len(y) + 2)
+            pNotWord = 1 - pWord
+            # p(word, 0) = p(0|word) * p(word)
+            pWordAnd0 = (c0[word] + 1) / (c0[word] + c1[word] + 2) * pWord
+            pWordAnd1 = (c1[word] + 1) / (c0[word] + c1[word] + 2) * pWord
+            # p(~word, 0) = p(0|~word) * p(~word) or p0 - p(word, 0)
+            pNotWordAnd0 = p0 - pWordAnd0
+            pNotWordAnd1 = p1 - pWordAnd1
+
+            mi[word] = pWordAnd0 * np.log(pWordAnd0 / (p0 * pWord)) + pWordAnd1 * np.log(pWordAnd1 / (p1 * pWord)) + \
+                       pNotWordAnd0 * np.log(pNotWordAnd0 / (p0 * pNotWord)) + \
+                       pNotWordAnd1 * np.log(pNotWordAnd1 / (p1 * pNotWord))
+
+        return [wordtoken[0] for wordtoken in mi.most_common(n)]
+
+        # Question 4 : Updating the function CreateVocabulary
+
+    def CreateVocabulary(self, xTrainRaw, yTrainRaw, numFrequentWords=0, numMutualInformationWords=0,
+                         supplementalVocabularyWords=[]):
         if self.vocabularyCreated:
-            raise UserWarning("Calling CreateVocabulary after the vocabulary was already created. Call ResetVocabulary to reinitialize.")
-            
+            raise UserWarning(
+                "Calling CreateVocabulary after the vocabulary was already created. Call ResetVocabulary to reinitialize.")
+
         # This function will eventually scan the strings in xTrain and choose which words to include in the vocabulary.
         #   But don't implement that until you reach the assignment that requires it...
-        
+        top_frequent_words = self.FindMostFrequentWords(
+            xTrainRaw, numFrequentWords)
+        top_mutual_info_words = self.FindTopWordsByMutualInformation(
+            xTrainRaw, yTrainRaw, numMutualInformationWords)
+        self.vocabulary = top_frequent_words + top_mutual_info_words
+
         # For now, only use words that are passed in
         self.vocabulary = self.vocabulary + supplementalVocabularyWords
         
         self.vocabularyCreated = True
-        
-    def _FeaturizeXForVocabulary(self, xRaw): 
+
+    def _FeaturizeXForVocabulary(self, xRaw):
         features = []
         
         # for each word in the vocabulary output a 1 if it appears in the SMS string, or a 0 if it does not
@@ -42,33 +103,33 @@ class SMSSpamFeaturize(object):
                 features.append(1)
             else:
                 features.append(0)
-                
+
         return features
 
     def _FeaturizeXForHandCraftedFeatures(self, xRaw):
         features = []
-        
+
         # This function can produce an array of hand-crafted features to add on top of the vocabulary related features
         if self.useHandCraftedFeatures:
             # Have a feature for longer texts
-            if(len(xRaw)>40):
+            if (len(xRaw) > 40):
                 features.append(1)
             else:
                 features.append(0)
 
             # Have a feature for texts with numbers in them
-            if(any(i.isdigit() for i in xRaw)):
+            if (any(i.isdigit() for i in xRaw)):
                 features.append(1)
             else:
                 features.append(0)
-            
+
         return features
 
     def _FeatureizeX(self, xRaw):
         return self._FeaturizeXForVocabulary(xRaw) + self._FeaturizeXForHandCraftedFeatures(xRaw)
 
     def Featurize(self, xSetRaw):
-        return [ self._FeatureizeX(x) for x in xSetRaw ]
+        return [self._FeatureizeX(x) for x in xSetRaw]
 
     def GetFeatureInfo(self, index):
         if index < len(self.vocabulary):
